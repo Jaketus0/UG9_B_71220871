3118c94c660b801e42031f03c7fb867c150d8302
Via Angelyca <117884184+Jaketus0@users.noreply.github.com>
2022-11-17 12:50:28 +0700

    Add files via upload

---
+++ 1_B_71220871.py
@@ -0,0 +1,6 @@
+print("========== ALAT PENGHITUNG LINGKARAN SENTOT ==========")
+diameter= int (input ("Masukkan Diameter (Meter) : "))
+keliling= 3.14*diameter
+print("Jarak yang harus ditempuh : ",round (keliling), "Meter")
+
+
---
+++ 2_B_71220871.py
@@ -0,0 +1,4 @@
+print("========== INI PROGRAM SEDERHANA ==========")
+angka = int (input ("Masukkan Angka yang ingin di Pangkatkan: "))
+hasil = angka**7
+print("Angka anda telah di pangkat kan 7! anda mendapat angka: ",hasil)
